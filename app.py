79ed458094158622a9b95e87dc593b00613d91bc
Santiago Mercado <santiagomercadob@gmail.com>
2024-06-25 13:57:04 -0500

    Añadido soporte para archivos .txt

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
+import os
 
 app = Flask(__name__)
 
@@ -29,10 +30,21 @@ def replace_entities(text, entities):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        input_text = request.form['input_text']
-        ner_results = nlp(input_text)
-        modified_text = replace_entities(input_text, ner_results)
-        return render_template('index.html', input_text=input_text, modified_text=modified_text, ner_results=ner_results)
+        if 'input_text' in request.form:
+            input_text = request.form['input_text']
+            ner_results = nlp(input_text)
+            modified_text = replace_entities(input_text, ner_results)
+            return render_template('index.html', input_text=input_text, modified_text=modified_text, ner_results=ner_results)
+        elif 'file' in request.files:
+            file = request.files['file']
+            if file.filename.endswith('.txt'):
+                file_content = file.read().decode('utf-8')
+                ner_results = nlp(file_content)
+                modified_text = replace_entities(file_content, ner_results)
+                modified_file_path = os.path.join('modified_text.txt')
+                with open(modified_file_path, 'w', encoding='utf-8') as f:
+                    f.write(modified_text)
+                return send_file(modified_file_path, as_attachment=True)
     return render_template('index.html', input_text='', modified_text='', ner_results=[])
 
 if __name__ == '__main__':
