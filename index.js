c5b6bc1f14a4aba4f934dbbcb271910b303663d5
SantiagoMB13 <santiagomercadob@gmail.com>
2024-07-23 16:54:35 -0500

    Añadi desplazamiento fijo de fechas

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ function secondaryReplacements(text, mode) {
     const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
     const phoneRegex = /(?<!\d)(\d\s*){10}(?=\s|\n|$)/g;
     const idRegex = /(\d\s*\.?\s*){5,}(?=\s|\n|$)/g;
-    const dateRegex = /\b\d{2}[-/]\d{2}[-/]\d{4}\b/g; // Se puede separar en 2 para los formatos dd/mm/yyyy y dd-mm-yyyy
+    const dateRegex = /\b\d{1,2}[-/]\d{1,2}[-/]\d{2,4}\b/g; // Se puede separar en 2 para los formatos dd/mm/yyyy y dd-mm-yyyy
     //Ahora mismo se consideran formatos incorrectos como dd-mm/yyyy o dd/mm-yyyy, pero por ahora no se considera un problema al ser un caso demasiado específico
     
     let emailReplacement, phoneReplacement, idReplacement, dateReplacement;
@@ -171,21 +171,30 @@ function secondaryReplacements(text, mode) {
         emailReplacement = faker.internet.email();
         phoneReplacement = faker.helpers.fromRegExp('[0-9]{10}');
         idReplacement = faker.string.numeric({ length: { min: 5, max: 12 }, allowLeadingZeros: false });
-        dateReplacement = faker.number.int({ min: 1, max: 31 }) + '/' + faker.number.int({ min: 1, max: 12 }) + '/' + faker.number.int({ min: 1900, max: 2024 });
+        text = text.replace(dateRegex, (match) => shiftDate(match, 3710)); // Se desplazan las fechas 3710 dias hacia atrás
     } else {
         emailReplacement = '[email]';
         phoneReplacement = '[celular]'; 
         idReplacement = '[id]';
-        dateReplacement = '[fecha]';
+        text = text.replace(dateRegex, '[fecha]');
     }
     
     text = text.replace(emailRegex, emailReplacement);
     text = text.replace(phoneRegex, phoneReplacement); 
     text = text.replace(idRegex, idReplacement);
-    text = text.replace(dateRegex, dateReplacement);
-    
     return text;
 }
+
+// Función para desplazar una fecha por un número dado de días
+function shiftDate(dateStr, days) {
+    const [day, month, year] = dateStr.split(/[-/]/).map(Number);
+    const date = new Date(year, month - 1, day);
+    date.setDate(date.getDate() - days);
+    const newDay = String(date.getDate()).padStart(2, '0');
+    const newMonth = String(date.getMonth() + 1).padStart(2, '0');
+    const newYear = date.getFullYear();
+    return `${newDay}-${newMonth}-${newYear}`;
+  }
 
 // Función para mostrar los resultados en la página
 function displayResults(replacedText, entities) {
